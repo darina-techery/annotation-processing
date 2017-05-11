a6e10ff0d180562175928788a460f58f6ae1b3ec
Darina Chernysheva <darina.chernysheva@techery.io>
2017-05-11 15:55:55 +0300

    Fix: device-specific actions class is not created automatically if parent class contains abstract methods

--- src/main/java/com/techery/qa/Validator.java
+++ src/main/java/com/techery/qa/Validator.java
@@ -2,10 +2,14 @@ package com.techery.qa;
 
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -19,6 +23,7 @@ class Validator {
 	private Types typeUtils;
 	private Messager messager;
 	private ProcessingEnvironment processingEnvironment;
+	private List<String> errorMessages;
 	static final Collector<CharSequence, ?, String> JOIN_BY_COMMA = Collectors.joining(", ");
 	static final String ERROR_MESSAGE_PREFIX = "[VALIDATION ERROR]:\n";
 	Validator(ProcessingEnvironment processingEnvironment) {
@@ -29,8 +34,12 @@ class Validator {
 	}
 
 	void validateActionsClasses(Set<TypeMirror> actionsClasses) {
+		errorMessages = new ArrayList<>();
+		Generator classGenerator = new Generator(processingEnvironment);
+		//get all classes marked as Actions
 		for (TypeMirror baseActionsClass : actionsClasses) {
 			String baseClassName = TypeResolver.getSimpleName(baseActionsClass);
+			//get all device-specific Actions class names
 			for (String prefix : DEVICE_PREFIXES) {
 				String deviceSpecificClassName = prefix + baseClassName;
 				String fullDeviceSpecificClassName = TypeResolver.getPackageName(baseActionsClass)
@@ -38,16 +47,34 @@ class Validator {
 				TypeElement deviceSpecificElement = elementUtils
 						.getTypeElement(fullDeviceSpecificClassName);
 				if (deviceSpecificElement == null) {
-					messager.printMessage(WARNING,
-							"Class [" + deviceSpecificClassName + "] was not found and will be created empty.\n\n");
-					new Generator(processingEnvironment).generateMissingActionsClass(baseActionsClass, deviceSpecificClassName);
+					//check if base actions class has abstract methods
+					Element baseActionsClassElement = typeUtils.asElement(baseActionsClass);
+					List<? extends Element> baseActionsClassMembers = elementUtils.getAllMembers((TypeElement) baseActionsClassElement);
+					for (Element element : baseActionsClassMembers) {
+						if (element.getModifiers().contains(Modifier.ABSTRACT)) {
+							errorMessages.add("Class [" + deviceSpecificClassName
+									+ "] was not found and cannot be created automatically, "
+									+ "because parent class [" + baseClassName
+									+ "] contains abstract method [" + element.getSimpleName() + "].\n"
+									+ "Please create this class and override method manually, or implement it in base class.");
+						}
+					}
+					if (errorMessages.isEmpty()) {
+						messager.printMessage(WARNING,
+								"Class [" + deviceSpecificClassName + "] was not found and will be created empty.\n\n");
+						classGenerator.generateMissingActionsClass(baseActionsClass, deviceSpecificClassName);
+					}
 				} else if (deviceSpecificElement.getSuperclass() == null
 							|| !typeUtils.isAssignable(deviceSpecificElement.asType(), baseActionsClass)) {
-					messager.printMessage(ERROR, ERROR_MESSAGE_PREFIX +
-							"Class [" + deviceSpecificClassName
+					errorMessages.add("Class [" + deviceSpecificClassName
 							+ "] should extend [" + baseActionsClass + "], but ["
 							+ deviceSpecificElement.getSuperclass() + "] found instead.\n\n");
 				}
+			}
+		}
+		if (!errorMessages.isEmpty()) {
+			for (String errorMessage: errorMessages) {
+				messager.printMessage(ERROR, errorMessage);
 			}
 		}
 	}
